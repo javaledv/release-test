cf43cd67e8b28a4a7cc3541839aa0d1c1f74d978
deenLee <dzianis.leshaniuk@instinctools.com>
2020-07-22 13:42:22 +0300

    asd

--- release.sh
+++ release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # CHANGE THESE BEFORE RUNNING THE SCRIPT!
-releaseVersion=5.0.5
-devmentVersion=5.0.6-SNAPSHOT
+releaseVersion=6.0.5
+devmentVersion=6.0.6-SNAPSHOT
 
 git checkout release/release-$releaseVersion
 
